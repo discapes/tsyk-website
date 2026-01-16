c42d6c8b609d517ab3b7a0b4ce08a1bef852045c
Miika Tuominen <miika.km.tuominen@gmail.com>
2026-01-16 16:09:48 +0200

    backend: remove caddyfiles and gitignore

--- tsyk-website/build.sh
+++
@@ -1,6 +0,0 @@
-set -euo pipefail
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 861821644425.dkr.ecr.eu-north-1.amazonaws.com
-docker build . -t 861821644425.dkr.ecr.eu-north-1.amazonaws.com/eliittilukio-frontend
-docker push 861821644425.dkr.ecr.eu-north-1.amazonaws.com/eliittilukio-frontend
-
-# on server, docker compose pull
