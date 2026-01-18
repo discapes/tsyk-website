8333baedd6602fbc2b6e12b62751c9fc108e8ca0
Miika Tuominen <miika.km.tuominen@gmail.com>
2026-01-18 15:29:24 +0200

    fix website

--- tsyk-website/src/routes/+page.server.ts
+++ tsyk-website/src/routes/+page.server.ts
@@ -2,7 +2,6 @@ const menuUrl =
 	"https://menu.kaarea.fi/KaareaAromieMenus/FI/Default/Kaarea/TSYKLU/Rss.aspx?Id=456d0033-1d0b-4926-b1b3-7fd105e1ae6a&DateMode=0";
 
 import type { PageServerLoad } from "./$types.js";
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 export const load: PageServerLoad = async ({ params }) => {
 	try {
 		const rss = await fetch(menuUrl).then((res) => res.text());
